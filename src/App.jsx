33649b45ade8f9fb4df26c857e8f4b6d43a17195
ishelp-lab <ishelp.adm@gmail.com>
2026-07-15 14:41:57 -0300

    Configura branch main para exibir apenas a tela de desenvolvimento

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import HomePage from './HomePage.jsx'
 import DevPage from './DevPage.jsx'
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/em-desenvolvimento" element={<DevPage />} />
-      </Routes>
-    </Router>
-  )
+  return <DevPage />
 }
 
 export default App
